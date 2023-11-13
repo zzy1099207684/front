92be06f698d5a61dcfeeef7d0b1fde06f337f088
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-11-14 00:46:44 +0200

    1111

--- JavaScript_Assignment/Module 1/10.js
+++ JavaScript_Assignment/Module 1/10.js
@@ -1,7 +1,7 @@
 // Make a program that asks the user for the number of dice and the sum of the eye numbers of interest to the user. The purpose of your program is now to find out with what probability the number of dice given by the user produces the sum of the number of eyes given by the user. For example, if the user enters 3 as the number of dice and 17 as the sum of the eyes, the program calculates the probability that the sum of the three dice's eye numbers is 17. (5p)
 
 // Solve the problem by simulating: Have the program roll a given number of dice in a for-loop (e.g. 10,000 times) and calculate what proportion of the repetitions produced the sum of eye numbers of interest to the user.
-
+'use strict';
 function main(){
     let diceNumber = parseInt(document.querySelector("#diceNumber").value);
     let eyeNumber = parseInt(document.querySelector("#eyeNumber").value);
